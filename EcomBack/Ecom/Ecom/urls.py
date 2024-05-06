cdf2efe91d154f6d44480c530792519bef3b3490
IsakGairatov <isak3141592653@gmail.com>
2024-05-06 22:31:39 +0500

    Made model category inside subapp category

--- EcomBack/Ecom/Ecom/urls.py
+++ EcomBack/Ecom/Ecom/urls.py
@@ -24,6 +24,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('api.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- EcomBack/Ecom/api/category/models.py
+++ EcomBack/Ecom/api/category/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=50)
+    description = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
