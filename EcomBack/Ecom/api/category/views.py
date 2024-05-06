18f25413a4e2cb3e997cf53c15f1bad076161789
IsakGairatov <isak3141592653@gmail.com>
2024-05-06 23:01:11 +0500

    Added serializer

--- EcomBack/Ecom/api/category/views.py
+++ EcomBack/Ecom/api/category/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+
+from .models import *
+from .serializers import *
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all().order_by('name')
+    serializer_class = CategorySerializer
